7b4ba07b192b9753e1402e159c23f240d5f34300
INNOCENT VIR <102230621+VIR99@users.noreply.github.com>
2022-08-16 10:34:56 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -108,7 +108,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(
 )
 # https://www.gbmb.org/mb-to-bytes
 
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION", "")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
